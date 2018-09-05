8d6b63c24acfaa4d60cbe8de48634bf6c7270030
Jskeodoakfjageukroquekrodowhrlfwk <42950704+Jskeodoakfjageukroquekrodowhrlfwk@users.noreply.github.com>
2018-09-05 23:40:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -579,19 +579,19 @@ if(!c) return;
 
 
 client.on("guildMemberAdd", member => {
-let welcomer = member.guild.channels.find("name","welcome");
+      const welcomer = bot.channels.get("479720488046952466");
       if(!welcomer) return;
       if(welcomer) {
          moment.locale('ar-ly');
          var h = member.user;
-        let norelden = new Discord.RichEmbed()
+         let embed = new Discord.RichEmbed()
         .setColor('RANDOM')
         .setThumbnail(h.avatarURL)
         .setAuthor(h.username,h.avatarURL)
-        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \` [ ${moment(member.user.createdAt).fromNow()} ]\``,true)            
-         .addField(': تاريخ دخولك السيرفر',`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')} \n\`\`[ ${moment(member.joinedAt).startOf(' ').fromNow()} ]\`\``, true)      
+        .addField(': تاريخ دخولك الدسكورد',`\`\`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')}\`\` **\n** **  ${moment(member.user.createdAt).fromNow()} **`,true)            
+         .addField(': تاريخ دخولك السيرفر',`\`\`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')}\`\` \n **${moment(member.joinedAt).startOf(' ').fromNow()} **`, true)      
          .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
-     welcomer.send({embed:norelden});          
+     welcomer.send(embed);          
                
  
       }
